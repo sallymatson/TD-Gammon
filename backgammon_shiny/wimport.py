87d9eedcbee1c6ac57f039916121f0633f64c25f
painef <paine.f@gmail.com>
2019-04-29 09:08:06 -0400

    more changes

--- backgammon_shiny/wimport.py
+++ backgammon_shiny/wimport.py
@@ -3,7 +3,7 @@ import pickle
 
 def read_file(file):
     with open(file,'rb') as f:
-        weights = pickle.load(f)
+        weights = pickle.load(f, encoding="latin1")
     return(weights)
 
 read_file("weights0.bin")
